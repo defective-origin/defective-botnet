a5be1d49f5cb44c0cb3ad75c323835768e25378b
defective <defective.origin@gmail.com>
2020-06-06 13:26:57 +0300

    feat: finish event emitter

--- src/event/event_emitter.py
+++ src/event/event_emitter.py
@@ -3,7 +3,6 @@ from .event import Event
 from .base_event import BaseEvent
 import re
 
-# TODO: set via decorator?
 class EventEmitter(BaseEvent):
     __events = {}
 
@@ -25,17 +24,33 @@ class EventEmitter(BaseEvent):
         """Return all names in string."""
         return re.sub(' +', ' ', names.strip()).split()
 
-    def on(self, names: str, callback: Callable) -> EventEmitter:
+    def on(self, names: str, callback: Callable = None) -> Union[EventEmitter, Callable]:
         """Add unlimited action to event."""
         return self.once(names, None, callback)
 
-    def once(self, names: str, times: int, callback: Callable) -> EventEmitter:
+    def once(self, names: str, times: int, callback: Callable = None) -> Union[EventEmitter, Callable]:
         """Add limited action to event."""
-        for name in self.__parse_names(names):
-            if not self.contains(name):
-                self.__events[name] = Event()
-            self.__events[name].once(times, callback)
+        def set_handler(callback: Callable):
+            for name in self.__parse_names(names):
+                if not self.contains(name):
+                    self.__events[name] = Event()
+                self.__events[name].once(times, callback)
+            return callback
+
+        if callback is None:
+            return set_handler
+        set_handler(callback)
         return self
+
+    def event(self, *args, **kwargs):
+        """Decorator to register an event handler."""
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            return self.on(args[0].__name__)(args[0])
+        else:
+            def set_handler(handler):
+                return self.on(handler.__name__, *args, **kwargs)(handler)
+
+            return set_handler
 
     def off(self, names: str, callback: Union[Callable, None] = None) -> EventEmitter:
         """Remove event by the name or remove only action from event if the callback was given."""
